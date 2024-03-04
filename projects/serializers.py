cd1c2e784ea6fc71a85445ac53615e9338596f02
aikhan <aikhanerboll@gmail.com>
2024-03-04 14:18:06 +0600

    course, project, task, pagination, filter, swagger

---
+++ projects/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+from .models import *
+
+class CourseSerializer(serializers.ModelSerializer):
+
+    status = serializers.CharField(read_only=True)
+
+    class Meta:
+        model = Course
+        fields = ['title', 'price']
+
+class ProjectSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Project
+        fields = ['title', 'description']
+
+class TaskSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Task
+        fields = ['title', 'description', 'user_answer']
+
+    def validate_user_answer(self, user_answer):
+        task = Task.objects.filter(user_answer=user_answer).first()
+        if task and task.is_user_answer_correct():
+            return 'Your answer if correct'
+        return 'Incorrect answer. Try again.'
