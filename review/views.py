15a102b9fe6d281c29a6fa030fab3b4a099f4f37
aikhan <aikhanerboll@gmail.com>
2024-03-07 13:59:16 +0600

    fixed permissions for comments likes and cart

--- review/views.py
+++ review/views.py
@@ -1,40 +1,54 @@
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.decorators import action
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from .models import Comment, Like, Rating, CartCourse, Cart
 from projects.models import Course
 from .serializers import CommentSerializer, LikeSerializer, RatingSerializer, CartCourseSerializer, CartSerializer
 from .permissions import IsAuthorPermission
 
-class CommentViewSet(viewsets.ModelViewSet):
+class PermissionMixin:
+    def get_permissions(self):
+        if self.action in ['list', 'retrieve']:
+            self.permission_classes = [AllowAny]
+        elif self.action == ['create']:
+            self.permission_classes = [IsAuthenticated]
+        elif self.action in ['update', 'partial_update', 'destroy']:
+            self.permission_classes = [IsAuthorPermission]
+        return super().get_permissions()
+
+class CommentViewSet(PermissionMixin, viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
-    permission_classes = [IsAuthorPermission]
 
-class LikeViewSet(viewsets.ModelViewSet):
+class LikeViewSet(PermissionMixin, viewsets.ModelViewSet):
     queryset = Like.objects.all()
     serializer_class = LikeSerializer
-    permission_classes = [IsAuthorPermission]
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
-class RatingViewSet(viewsets.ModelViewSet):
+class RatingViewSet(PermissionMixin, viewsets.ModelViewSet):
     queryset = Rating.objects.all()
     serializer_class = RatingSerializer
-    permission_classes = [IsAuthorPermission]
     
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
 class CartViewSet(viewsets.ModelViewSet):
     serializer_class = CartSerializer
-    permission_classes = [IsAuthorPermission]
 
     def get_queryset(self):
         user = self.request.user
         queryset = Cart.objects.filter(user=user).order_by('updated_at')
         return queryset
+    
+    def get_permissions(self):
+        if self.action == ['create']:
+            self.permission_classes = [IsAuthenticated]
+        else:
+            self.permission_classes = [IsAuthorPermission]
+        return super().get_permissions()
     
     @action(detail=True, methods=['post'])
     def add_to_cart(self, request, pk=None):
