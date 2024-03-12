1e35c8985fb47c8a798f2f54cad9063e67575ada
aikhan <aikhanerboll@gmail.com>
2024-03-12 14:15:02 +0600

    made rating to 5 not to 10

--- cart/tasks.py
+++ cart/tasks.py
@@ -14,12 +14,12 @@ def send_order_details(email, order, verification_code):
         [email]
     )
 
-@app.task
-def handle_payment_intent_succeeded(payment_intent_id):
-    try:
-        order = Order.objects.get(payment_intent_id=payment_intent_id)
-        order.is_paid = True
-        order.save()
-        print('Payment intent succeeded for Order ID:', order.id)
-    except Order.DoesNotExist:
-        print('Order not found for Payment Intent ID:', payment_intent_id)
+# @app.task
+# def handle_payment_intent_succeeded(payment_intent_id):
+#     try:
+#         order = Order.objects.get(payment_intent_id=payment_intent_id)
+#         order.is_paid = True
+#         order.save()
+#         print('Payment intent succeeded for Order ID:', order.id)
+#     except Order.DoesNotExist:
+#         print('Order not found for Payment Intent ID:', payment_intent_id)
